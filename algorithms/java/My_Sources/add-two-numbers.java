059a5021ca0d89d4b9eea1f4fa8e73f82b83d371
Junaid Ali <junaid_ali4@yahoo.com>
2016-12-26 00:07:02 +0500

    Add Two Numbers:
      You are given two linked lists representing two non-negative numbers.
      The digits are stored in reverse order and each of their nodes contain
      a single digit. Add the two numbers and return it as a linked list.
    
      Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
      Output: 7 -> 0 -> 8
    
    Signed-off-by: Junaid Ali <junaid_ali4@yahoo.com>

---
+++ algorithms/java/My_Sources/add-two-numbers.java
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int num1 = nodeToNumber(reverseList(l1));
+        int num2 = nodeToNumber(reverseList(l2));
+        if (num1 == -1 || num2 == -1 )
+            return null;
+        int sum = num1 + num2;
+        return numberToNode(sum);
+    }
+    
+    public ListNode reverseList(ListNode node) {
+        ListNode reversed = null;
+        while (node != null) {
+            ListNode temp = new ListNode(node.val);
+            temp.next = reversed;
+            reversed = temp;
+            node = node.next;
+        }
+        return reversed;
+    }
+    
+    public int nodeToNumber(ListNode node) {
+        if (node == null) return -1; //Error
+        int number = node.val;
+        while(node.next != null) {
+            node = node.next;
+            number *= 10;
+            number += node.val;
+        }
+        return number;
+    }
+    
+    public ListNode numberToNode(int number) {
+        ListNode sum = null;
+        int digit;
+        
+        //Convert number into digits and push into linkedlist
+        while (number > 0) {
+            digit = number%10;
+            number = number/10;
+            ListNode temp = new ListNode(digit);
+            temp.next = sum;
+            sum = temp;
+        }
+        
+        //number is stored in reverse order, push into another linkedlist to re-order and return
+        return reverseList(sum);
+    }
+}
